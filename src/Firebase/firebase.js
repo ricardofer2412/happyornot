d2d4c07dd9472ff33cbe79fa85f3b0f953beb68e
Richard Casanova <ricardofer2412@hotmail.com>
2020-08-05 16:51:58 -0400

    add faces

---
+++ src/Firebase/firebase.js
@@ -0,0 +1,20 @@
+import * as firebase from 'firebase'
+import "firebase/auth"
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD3EGqIGqnTbTgO8jQDWLuLclJyd0s9-PA",
+  authDomain: "happyornot-569d4.firebaseapp.com",
+  databaseURL: "https://happyornot-569d4.firebaseio.com",
+  projectId: "happyornot-569d4",
+  storageBucket: "happyornot-569d4.appspot.com",
+  messagingSenderId: "224112296329",
+  appId: "1:224112296329:web:9bda3f5b06e1e22b3ca158",
+  measurementId: "G-KYCN2LSXH2"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
+
+export const auth = firebase.auth()
+export default firabase;
