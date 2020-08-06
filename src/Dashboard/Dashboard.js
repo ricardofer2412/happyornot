a0f88f021fdd13f98deb7739c2f5a302b280ee3e
Richard Casanova <ricardofer2412@hotmail.com>
2020-08-06 14:47:53 -0400

    add dashboard

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -1,11 +1,39 @@
 import React from 'react'
+import Navbar from '../Navbar/Navbar'
+import firebase from '../Firebase/firebase'
 
 class Dashboard extends React.Component {
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      happyCount: '',
+      notHappyCount: '',
+    }
+  }
+  getHappyCount() {
+    const happyCount = []
+    firebase.firestore().collection("ratingCount").doc('happyCount')
+      .get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          console.log(doc.data())
+          happyCount.push({
+            key: doc.id,
+            happyCount
+
+          })
+        })
+      })
+    this.setState({
+      happyCount
+    })
+    console.log('This is happy counts' + this.state.happyCount)
+  }
   render() {
     return (
       <div>
-        Dashboard
+        <Navbar />
+        <h1>{this.state.happyCount}</h1>
       </div>
     )
   }
